f8fbcd93ccb97ddcdd4bf9642169d4873f26f43d
Onesimus Addo Appiah <draddo11@gmail.com>
2024-02-02 21:02:53 +0000

    created routes , user and user service files

---
+++ src/routes.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import httpStatus from "http-status";
+import appRoutes from './routes';
+
+//import {StatusCodes} from 'http-status-codes';
+
+
+const router = express.Router();
+const port = 3000;
+
+//const status= require('http-status');
+
+// http-status
+const STATUS={
+    SUCCESSFUL:'OK',
+    FAILURE:'NO'
+}
+
+//router.use(express.json());
+//app.use('/v1', approutes);
+
+router.get('/CARS',  (req, res)=> {
+    res.status(httpStatus.OK);
+    res.send("Hello world i am here X 2");
+});
+
+router.post('/add' ,(req,res)=> {
+    const data = [];
+    const{body} =req;
+    //data.push(req.body);
+    if (!body.name){
+        return res.status(httpStatus.BAD_REQUEST).send({
+            status: STATUS.FAILURE,
+            message: 'Name is required',
+        });
+    }
+    return  res.status(httpStatus.CREATED).send({
+        status: STATUS.SUCCESSFUL,
+        message: data,
+    })
+});
+
+export default router;
